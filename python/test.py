cfcfb70284409edd3bcaf89930a310a34b172674
Kenji Ogura <k5i.ogura.v40@gmail.com>
2019-05-06 16:10:09 +0000

    revise

--- python/test.py
+++ python/test.py
@@ -10,21 +10,25 @@ import re
 from pdb import *
 
 args = argparse.ArgumentParser()
-args.add_argument('-d','--depth', dest="color",action='store_false')
+args.add_argument('-d','--depth', action='store_true')
+args.add_argument('-c','--color', action='store_true')
+args.add_argument('-b','--both' , action='store_true')
 args.add_argument('-cw', type=int, default=640)
 args.add_argument('-ch', type=int, default=480)
 args.add_argument('-vw', type=int, default=640)
 args.add_argument('-vh', type=int, default=480)
 args = args.parse_args()
+if args.both or (not args.color and not args.depth): args.depth = args.color = True
+print("Depth/Color/Both",args.depth, args.color, args.both)
 
 pipeline = rs.pipeline()
-config = rs.config()
+config   = rs.config()
 
 align_to = rs.stream.color
 align    = rs.align(align_to)
 
-if not args.color: config.enable_stream(rs.stream.depth, args.cw, args.ch, rs.format.z16, 30)
-if     args.color: config.enable_stream(rs.stream.color, args.cw, args.ch, rs.format.bgr8, 30)
+if args.depth: config.enable_stream(rs.stream.depth, args.cw, args.ch, rs.format.z16, 30)
+if args.color: config.enable_stream(rs.stream.color, args.cw, args.ch, rs.format.bgr8, 30)
 
 # Start streaming
 pipeline.start(config)
@@ -33,17 +37,16 @@ images= 0
 while True:
     frame = pipeline.wait_for_frames()
     align_frame = align.process(frame)
-    if args.color:
-        color_frame = align_frame.get_color_frame()
-    else:
-        depth_frame = align_frame.get_depth_frame()
+    if args.color: color_frame = align_frame.get_color_frame()
+    if args.depth: depth_frame = align_frame.get_depth_frame()
 
-    if not args.color:
+    if not args.color and args.depth:
         depth_frame = rs.colorizer().colorize(depth_frame)
         im = np.asanyarray(depth_frame.get_data())
-        #im = cv2.applyColorMap(depth_image, cv2.COLORMAP_HSV)
         im = cv2.resize(im, (args.vw,args.vh))
-    else:
+    if args.color and args.depth:
+        depth_frame = rs.colorizer().colorize(depth_frame)
+    if args.color:
         color_image = np.asanyarray(color_frame.get_data())
         im = cv2.resize(color_image, (args.vw, args.vh))
 
